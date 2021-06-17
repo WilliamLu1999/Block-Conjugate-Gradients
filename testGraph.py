14911b884023dd1d4b0a03b5ecc4600b17be5fb6
William Lu <weilianlu@gmail.com>
2021-06-17 16:01:36 +0800

    Redo graphs

--- testGraph.py
+++ testGraph.py
@@ -2,8 +2,10 @@ from CG import CG, is_pos_def, bounds,cond_size,create_matrix,find_cond_num,cond
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+
+from scipy.interpolate import make_interp_spline
 # Setting up:
-n =10
+n = 80
 C = create_matrix(n)
 
 #condition num of C
@@ -17,46 +19,61 @@ x0 = np.random.randint(10,size =(n,1))
 xx = CG(C,x0,b1,1e-6)
 xxx = np.asarray(xx) # convert tuple to array
 xr = np.linalg.solve(C,b1)
-size_list = range(2,50)
-cond_list = []
-for n in range(2,50):
-    M = create_matrix(n)
-    M_cond = find_cond_num(M)
-    cond_list.append(M_cond)
-
-plt.figure(figsize=(10, 5))
-# calling the function
-cond_size(cond_list, size_list)
-
-plt.plot(cond_list, size_list , label ="line 1")
-plt.xlabel("kappa the condition number")
-plt.ylabel("n the size of the matrix")
-plt.title("Relationship between n and kappa")
-plt.legend()
-
 
 
 # Theory error plotting
 C_x_0 = C.dot(x0) 
-energy_norm_0 = math.sqrt(np.dot(x0.T,C_x_0)) #get A norm of error of initial guess
-iteration_k = np.linspace(0,10,10)
-energy_norm_k = 2*pow((((math.sqrt(kappa) -1))/(math.sqrt(kappa)+1)),iteration_k)*energy_norm_0
+energy_norm_0 = math.sqrt(np.dot(x0.T,C_x_0)) #get A norm of the error of initial guess
+iteration_k = list(range(0,150))
+enerygy_norm_k_list = []
+for k in iteration_k:
+    energy_norm_k = 2*pow((((math.sqrt(kappa) -1))/(math.sqrt(kappa)+1)),k)*energy_norm_0
+    enerygy_norm_k_list.append(energy_norm_k)
 
+# Convert each error norm to log10 base  
+log_energy1 = [math.log10(k) for k in enerygy_norm_k_list]
+X_Y_Spline =make_interp_spline(iteration_k,log_energy1)
+iteration = np.linspace(0,max(iteration_k),500)
+Y = X_Y_Spline(iteration)
 
-
-
-
-
-fig, ax = plt.subplots()
-plt.plot(iteration_k,energy_norm_k)
+plt.plot(iteration,Y,label ='theoretical convergence')
 plt.xlabel("iteration")
-plt.ylabel("error theoretical")
-plt.title("Relationship error and iteration")
+plt.ylabel("error theoretical log10 base")
+plt.title("Relationship between error and iteration")
 
 
-
-# our error plotting
-
+# our error
 our_err = find_error(C,x0,b1,1e-6)
-plt.plot(our_err,'ro')
+
+log_energy2= [math.log10(j) for j in our_err]
+
+iteration2 = list(range(0,len(our_err)))
+plt.plot(iteration2,log_energy2,label ='our line')
+plt.legend()
 plt.show()
+
+plt.savefig('errorComparison.pgf')
+
+
+
+# different condition number different rate
+
+ # decide the matrix size
+n  = np.random.randint(2,100)
+# create the matrix
+
+for j in range(1,5):
+    R = create_matrix(n)
+    KAP = find_cond_num(R)
+    b2 = np.random.randint(10,size =(n,1))
+    x00 = np.random.randint(10,size =(n,1))
+    us_error = find_error(R,x00,b2,1e-6)
+    log_energy3= [math.log10(l) for l in us_error]
+    iteration3 = list(range(0,len(us_error)))
+    plt.plot(iteration3,log_energy3,label =KAP)
+    plt.legend()
+plt.xlabel("iteration")
+plt.ylabel("error log10 base")
+plt.title("Different condition number (left corner) causes different convergence rate")
+plt.show()
+plt.savefig('condtion number comparison.pgf')
