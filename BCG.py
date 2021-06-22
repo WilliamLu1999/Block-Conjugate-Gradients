d171dea0eacff07a766da9b48ebed0afa555ef06
William Lu <weilianlu@gmail.com>
2021-06-22 15:47:05 +0800

    update

--- BCG.py
+++ BCG.py
@@ -3,42 +3,41 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import scipy.sparse.linalg
-def BCG(A,X,B):
+def BCG(A,X,B,tol =1e-6):
     R = B - A@X #Residual
     P = R #Search direction
     k = 0 #iteration
     R_old = R.T@R #denominator(for easier calculation later)
-    while True:
+    while R.all()!=0:
+    
         A_P =A@P
         P_A_P_inv = np.linalg.pinv(P.T@A_P)
         # Lam is the step size matrix
         Lam = P_A_P_inv @ R_old
         X = X + P@Lam
         R = R - A@(P@Lam)
-        if np.linalg.norm(R) <= 1e-6:
+        if np.linalg.norm(R) <= tol:
             break
         else:
             R_T_R = R.T@R
             Phi=  np.linalg.pinv(R_old)@R_T_R
-
             P = R+ P@Phi
             R_old = R.T@R #update the denominator to be the numerator's value for the next fraction
-            k += 1
+            k+=1
     return X  
 
 def create_NM(n,m): #n is the size of the matrix A; m is the number of columns of intial guess
     X = np.random.uniform(0,10,(n,m))
     return X
 
-
-def find_Frobenius(A,X,B):
+def find_Frobenius(A,X,B,tol =1e-6):
     R = B - A@X #Residual
     P = R #Search direction
-    k = 0 #iteration
     R_old = R.T@R #denominator(for easier calculation later)
     Xr = np.linalg.solve(A,B)
     err_F_list= []
-    while True:
+    counter = 0
+    while R.all()!= 0:
         A_P =A@P
         P_A_P_inv = np.linalg.pinv(P.T@A_P)
         # Lam is the step size matrix
@@ -51,15 +50,14 @@ def find_Frobenius(A,X,B):
         Xe_F_norm = np.linalg.norm(Xe)
         err_F_list.append(Xe_F_norm)
 
-        if np.linalg.norm(R) <= 1e-6:
-            break        
+        if np.linalg.norm(R)<=tol:
+            break
         else:
             R_T_R = R.T@R
             Phi=  np.linalg.pinv(R_old)@R_T_R
-
             P = R+ P@Phi
             R_old = R.T@R #update the denominator to be the numerator's value for the next fraction
-            k += 1
+            counter+=1
     return err_F_list
 
 
--- BCGtest.py
+++ BCGtest.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 import scipy.sparse.linalg
 
 # Setup
-A = create_matrix(100)
-X = create_NM(100,5)
-B = create_NM(100,5)
+A = create_matrix(10)
+X = create_NM(10,5)
+B = create_NM(10,5)
 # Testing
-XK = BCG(A,X,B)
+XK = BCG(A,X,B,1e-6)
 XR =np.linalg.solve(A,B)
 if(XK.all()==XR.all()):
     print('Your BCG converges to the right solution.')
@@ -21,17 +21,20 @@ if(XK.all()==XR.all()):
 n  = np.random.randint(2,100)
 # create matrix W
 W = create_matrix(n)
-for j in range(1,5):
-    blk_size = np.random.randint(2,100) # block size
+F_list= []
+for j in range(1,10):
+    blk_size = np.random.randint(2,800) # block size
     BB = create_NM(n,blk_size) # Righ hand side matrix B
     XX = create_NM(n,blk_size) # Guessed initial solution X
-    F_list =find_Frobenius(A,X,B)
+    F_list = find_Frobenius(A,X,B,1e-6)
     log_Frobenius =[math.log10(f) for f in F_list]
     iteration4 = list(range(0,len(F_list)))
     plt.plot(iteration4,log_Frobenius,label = blk_size)
     plt.legend()
+#print(len(F_list))
 plt.xlabel("iteration")
 plt.ylabel("error log10 base")
-plt.title("Block size versus convergence speed")
+plt.title("Block size versus convergence speed")    
 plt.show()
 plt.savefig('blockCS.pgf')
+
