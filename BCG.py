0c4034d174a1847fd8551669997b1e5298d689d9
William Lu <weilianlu@gmail.com>
2021-06-26 18:15:00 +0800

    two new graphs

--- BCG.py
+++ BCG.py
@@ -33,14 +33,14 @@ def create_NM(n,m): #n is the size of the matrix A; m is the number of columns o
     X = np.random.uniform(0,10,(n,m))
     return X
 
-def find_Frobenius(A,X,B,tol =1e-6):
+def find_Frobenius(A,X,B,tol =1e-6,maxiter= None):
     R = B - A@X #Residual
     P = R #Search direction
     R_old = R.T@R #denominator(for easier calculation later)
     Xr = np.linalg.solve(A,B)
     err_F_list= []
     counter = 0
-    while R.all()!= 0:
+    while True:
         A_P =A@P
         P_A_P_inv = np.linalg.pinv(P.T@A_P)
         # Lam is the step size matrix
@@ -49,8 +49,10 @@ def find_Frobenius(A,X,B,tol =1e-6):
         R = R - A@(P@Lam)
 
         # Find the error and the Frobenius norm
-        Xe = X - Xr
-        Xe_F_norm = np.linalg.norm(Xe)
+        Xe =(Xr-X).T@A@(Xr-X)
+        
+        Xe_F_norm = math.sqrt(Xe.trace())
+
         err_F_list.append(Xe_F_norm)
 
         if np.linalg.norm(R)<=tol:
@@ -61,6 +63,9 @@ def find_Frobenius(A,X,B,tol =1e-6):
             P = R+ P@Phi
             R_old = R.T@R #update the denominator to be the numerator's value for the next fraction
             counter+=1
+            if counter>=maxiter:
+                break
+         
     return err_F_list
 
 
--- BCGtest.py
+++ BCGtest.py
@@ -1,4 +1,4 @@
-from CG import create_matrix
+from CG import create_matrix, CG, is_pos_def, bounds,cond_size,create_matrix,find_cond_num,cond_size,find_error
 from BCG import BCG,create_NM,find_Frobenius
 import numpy as np
 import math
@@ -17,24 +17,42 @@ if(XK.all()==XR.all()):
 
 ##############################################################
 # block size vs convergence
-# decide the matrix size
-n  = np.random.randint(2,100)
-# create matrix W
+# decide the matrix size. create matrix W
+n  = 80
 W = create_matrix(n)
-F_list= []
-for j in range(1,10):
-    blk_size = np.random.randint(2,800) # block size
-    BB = create_NM(n,blk_size) # Righ hand side matrix B
-    XX = create_NM(n,blk_size) # Guessed initial solution X
-    F_list = find_Frobenius(A,X,B,1e-6)
-    log_Frobenius =[math.log10(f) for f in F_list]
-    iteration4 = list(range(0,len(F_list)))
-    plt.plot(iteration4,log_Frobenius,label = blk_size)
-    plt.legend()
-#print(len(F_list))
+
+# Compare block size 1 in BCG to CG
+# First testing: x0 is the initial guess, b1 is a block size of 1 vector
+x0 = np.random.randint(10,size =(n,1))
+b1 = np.random.randint(10,size =(n,1))
+cg_err = find_error(W,x0,b1,1e-6)
+log_energy_cg= [math.log10(j) for j in cg_err]
+iteration_cg = list(range(0,len(cg_err)))
+plt.plot(iteration_cg,log_energy_cg,label ='cg line')
+
+bcg_err =find_Frobenius(W,x0,b1,1e-6,2*n)
+log_energy_bcg= [math.log10(p) for p in bcg_err]
+iteration_bcg = list(range(0,len(bcg_err)))
+plt.plot(iteration_bcg,log_energy_bcg,label ='bcg line')
+plt.legend()
 plt.xlabel("iteration")
 plt.ylabel("error log10 base")
-plt.title("Block size versus convergence speed")    
+plt.title("BCG versus CG of block size 1")   
 plt.show()
-plt.savefig('blockCS.pgf')
+print(cg_err)
+print(bcg_err)
+###############################################################
+# Block size of 2,4,8,16
+for expo in range(1,5):
+    X0 = np.random.randint(10,size=(n,2**expo))
+    B1 = np.random.randint(10,size=(n,2**expo))
+    BCG_err = find_Frobenius(W,X0,B1,1e-6,2*n)
+    log_frob_BCG = [math.log10(q) for q in BCG_err]
+    iteration_BCG = list(range(0,len(BCG_err)))
+    plt.plot(iteration_BCG,log_frob_BCG,label =2**expo)
+    plt.legend()
+plt.xlabel("iteration")
+plt.ylabel("error log10 base")
+plt.title("BCG: different block size")   
+plt.show()
 
